97141670b255aa17ace9099b624fcbe9e5ae221e
Michael Williams <mike@pentangle.net>
2014-05-03 20:06:59 -0400

    def randomdata

--- kmeans.py
+++ kmeans.py
@@ -1,6 +1,34 @@
 import numpy as np
 
 
+def randomdata(K=2, m=100, ndim=2, sigma=0.5):
+    """Generate random data for use in testing K-means algorithm.
+    """
+
+    # c will hold cluster assignment for each of m points
+    c = np.zeros(m)
+    # centroid will hold location of K centroids
+    centroids = np.zeros([K, ndim])
+    # x will hold location of m randomly generated data points, distributed
+    # about the K centroids
+    x = np.zeros([m, ndim])
+
+    points_per_cluster = np.floor(m/K)
+    # For each cluster
+    for i in range(0, K):
+        # Allocate m points to K clusters
+        c[i*points_per_cluster:(i+1)*points_per_cluster] = i
+        # Generage K n-dimensional centroids
+        centroids[i, :] = np.random.choice(range(K), ndim, replace=False)
+
+    # For each data point
+    for i in range(0, m):
+        x[i, :] = centroids[c[i], :]
+        x[i, :] += np.random.normal(0, sigma, ndim)
+
+    return x
+
+
 def clusterassign(x, u):
     """
     For an array of centroids u in an n-dimensional space, return the index
@@ -18,8 +46,7 @@ def clustercentroid(x):
 
 
 def choosestartingpoint(x, K):
-    """
-    Randomly choose K points from the array x of n-dimensional points
+    """Randomly choose K points from the array x of n-dimensional points
     """
     npoints = x.shape[0]
     i = np.random.choice(npoints, K, replace=False)
