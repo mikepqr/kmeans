e2998028eb04672fc645610e63ee82cda7bf80c2
Michael Williams <mike@pentangle.net>
2014-05-03 23:03:26 -0400

    improved ploting, added distortion elbow demo

--- kmeans.py
+++ kmeans.py
@@ -65,12 +65,14 @@ def calcdistortion(x, c, u):
 
 def plotkmeans(x, c, centroids):
     """Scatter plot cluter centroids and data coded by cluster allocation"""
-    markers = ['s', 'o', 'h', '+']
-    colors = ['red', 'blue', 'green', 'cyan']
-    for i in range(0, centroids.shape[0]):
-        plt.scatter(x[c == i].T[0], x[c == i].T[1],
-                    marker=markers[i], color=colors[i])
-    plt.scatter(centroids.T[0], centroids.T[1], marker='*')
+    if x.shape[1] == 2:
+        plt.scatter(x.T[0], x.T[1], c=c, cmap=plt.cm.Set2, alpha=0.5)
+        plt.scatter(centroids.T[0], centroids.T[1], marker='*', s=100)
+    elif x.shape[1] == 3:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1, projection='3d')
+        ax.scatter(x.T[0], x.T[1], x.T[2], c=c, cmap=plt.cm.Set2)
+        ax.scatter(centroids.T[0], centroids.T[1], centroids.T[2], marker='*')
 
 
 def kmeans(x, K, n=10):
@@ -85,14 +87,14 @@ def kmeans(x, K, n=10):
         centroids = choosestartingpoint(x, K)
         newcentroids = centroids * 0.
 
-        # Repeat until centroids no longer moving
         while True:
+            # For every data point, determine cluster allocation
             for i in range(0, m):
                 c[i] = clusterassign(x[i], centroids)
-
+            # Determine new centroids based on new allocations
             for i in range(0, K):
                 newcentroids[i] = np.mean(x[c == i], 0)
-
+            # Repeat until centroids no longer moving
             if np.mean(abs(newcentroids - centroids)) < 0.01:
                 break
             else:
@@ -110,14 +112,26 @@ def kmeans(x, K, n=10):
             lowestdistortion = distortion
             bestcentroids = centroids
 
-    # Assign to final cluster locations
+    # For every data point, determine final cluster allocation
     for i in range(0, m):
         c[i] = clusterassign(x[i], bestcentroids)
 
-    return bestcentroids, c
+    return bestcentroids, c, lowestdistortion
 
 
-def demo(m=1000, K=4):
-    x = randomdata(K=K, m=m)
-    centroids, c = kmeans(x, K)
+def demo(m=1000, K=4, ndim=2):
+    x = randomdata(K=K, m=m, ndim=ndim)
+    centroids, c, lowestdistortion = kmeans(x, K)
     plotkmeans(x, c, centroids)
+    print centroids
+
+
+def demo_elbow():
+    maxk = 10
+    tryk = np.arange(1, maxk)
+    x = randomdata()
+    distortions = tryk * 0.
+    for i, thisk in enumerate(tryk):
+        centroids, c, distortions[i] = kmeans(x, K=thisk)
+
+    plt.plot(tryk, distortions)
